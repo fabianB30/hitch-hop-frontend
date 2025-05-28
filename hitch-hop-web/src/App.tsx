17eec4785f869dd26962cb90ad67d5e56a8a8683
C-Espriella13 <cahucaes05@gmail.com>
2025-05-28 12:42:46 -0600

    Init commit
    Added the necessary classes to start building

--- hitch-hop-web/src/App.tsx
+++ hitch-hop-web/src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css'
-import Router from './router/Router'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProfileSettings from './components/ProfileSettings';
+import './App.css';
+
 
 function App() {
-
-
   return (
-    <>
-      <Router />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/profile-settings" element={<ProfileSettings />} />
+        <Route path="/" element={<div>Aquí irá el contenido de la pantalla principal</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ hitch-hop-web/src/components/ProfileSettings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Imagen1 from '../assets/1.6-DefaultPFP.png';
+//import { Sidebar } from './shared/Sidebar';
+import { Button } from './ui/button';
+import { Checkbox } from './ui/checkbox';
+
+const ProfileSettings: React.FC = () => {
+    return (
+        
+    )
+}
+
+export default ProfileSettings;
