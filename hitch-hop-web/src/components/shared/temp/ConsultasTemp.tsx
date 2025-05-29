21790e71745e2807bbef0e09b07f5cf8729a9ecf
fabianB30 <fabianbustosv@gmail.com>
2025-05-29 00:17:17 -0600

    new: Sidebar start, color vars, fonts, etc.
    Bases de cosas visuales

---
+++ hitch-hop-web/src/components/shared/temp/ConsultasTemp.tsx
@@ -0,0 +1,6 @@
+export default function ConsultasTemp(){
+    return (
+        <h1> Consultas Temporal</h1>
+    )
+}
+
---
+++ hitch-hop-web/src/components/shared/temp/PerfilTemp.tsx
@@ -0,0 +1,6 @@
+export default function PerfilTemp(){
+    return (
+        <h1> Perfil Temporal</h1>
+    )
+}
+
