0628533d0156101205eaa7502f8fc1f9b9e02e11
Mariano Mayorga <mayorga.halabi@gmail.com>
2025-06-02 15:33:34 -0600

    prueba layout

--- hitch-hop-movil/app/(tabs)/vehiculos/_layout.tsx
+++ hitch-hop-movil/app/(tabs)/vehiculos/_layout.tsx
@@ -1,12 +1,47 @@
-import { Stack } from 'expo-router';
+import { Tabs } from "expo-router";
+import { Platform } from "react-native";
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
-export default function VehiculosLayout() {
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Vehículos' }} />
-      <Stack.Screen name="agregarVehiculo" options={{ title: 'Agregar Vehículo' }} />
-      <Stack.Screen name="editarVehiculo" options={{ title: 'Editar Información' }} />
-      <Stack.Screen name="informacionVehiculo" options={{ title: 'Información' }} />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Transparent background on iOS to show blur
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+      initialRouteName="sinVehiculos"
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
