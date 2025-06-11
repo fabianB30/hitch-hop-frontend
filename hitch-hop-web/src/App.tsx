dc7defb756074bfe80d7b4aaab48cb778afd2f7e
Gerald <geraldcalderon016@gmail.com>
2025-06-11 15:47:30 -0600

    Interconexión en la parte web

--- hitch-hop-web/src/App.tsx
+++ hitch-hop-web/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Router from './router/Router'
-
+import {AuthProvider} from './Context/auth-context';
 
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Router />
-    </>
+    </AuthProvider>
   );
 }
 
---
+++ hitch-hop-web/src/interconnection/place.tsx
@@ -0,0 +1,83 @@
+import axios from './axios';
+
+export const registerPlaceRequest = async (data : { 
+    name, description, longitude, latitude }): Promise<IJwtResponse | null> => {
+    try {
+        const res = await axios.post(`/backend/place/register`, data);
+        const dataPlace = res.data.data;
+        if (dataPlace) {
+            return dataPlace;
+        } else {
+            console.error('Invalid response structure:', res);
+            return null;
+        }
+    } catch (error) {
+        console.error('http request error: ', error);
+        return null;
+    }
+};
+
+export const getAllPlacesRequest = async (): Promise<IJwtResponse | null> => {
+    try {
+        const res = await axios.get(`/backend/place/get-all`);
+        const dataPlaces = res.data.data;
+        if (dataPlaces) {
+            return dataPlaces;
+        } else {
+            console.error('Invalid response structure:', res);
+            return null;
+        }
+    } catch (error) {
+        console.error('http request error: ', error);
+        return null;
+    }
+};
+
+export const getPlaceByIdRequest = async (id): Promise<IJwtResponse | null> => {
+    try {
+        const res = await axios.get(`/backend/place/get/${id}`, {params: {id}});
+        const dataPlace = res.data.data;
+        if (dataPlace) {
+            return dataPlace;
+        } else {
+            console.error('Invalid response structure:', res);
+            return null;
+        }
+    } catch (error) {
+        console.error('http request error: ', error);
+        return null;
+    }
+};
+
+export const updatePlaceRequest = async (id: string, data : { 
+    name, description, longitude, latitude }): Promise<IJwtResponse | null> => {
+    try {
+        const res = await axios.put(`/backend/place/update/${id}`, data, {params: {id}});
+        const dataPlace = res.data.data;
+        if (dataPlace) {
+            return dataPlace;
+        } else {
+            console.error('Invalid response structure:', res);
+            return null;
+        }
+    } catch (error) {
+        console.error('http request error: ', error);
+        return null;
+    }
+};
+
+export const deletePlaceRequest = async (id): Promise<IJwtResponse | null> => {
+    try {
+        const res = await axios.delete(`/backend/place/delete/${id}`, {params: {id}});
+        const dataPlace = res.data.data;
+        if (dataPlace) {
+            return dataPlace;
+        } else {
+            console.error('Invalid response structure:', res);
+            return null;
+        } 
+    } catch (error) {
+        console.error('http request error: ', error);
+        return null;
+    }
+};
