db0bf82185aef336696695131ada015607caa0a8
Saymon Porras Briones <118936420+CtpN3m01@users.noreply.github.com>
2025-06-11 18:16:15 -0600

    feat: add assets para ventana Bienvenida

---
+++ hitch-hop-movil/app/(tabs)/BienvenidaConductor/C_bienvenida.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CBienvenidaScreen() {
+    return (
+        <View>
+            <Text>Pantalla de Bienvenida Conductor</Text>
+        </View>
+    );
+}
---
+++ hitch-hop-movil/app/(tabs)/Register/components/TyC.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function TyCScreen() {
+    return (
+        <View>
+            <Text>Pantalla Terminos y condiciones</Text>
+        </View>
+    );
+}
